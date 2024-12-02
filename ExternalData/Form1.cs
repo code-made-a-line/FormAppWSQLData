ddaf2359fa78254f007cc474b729003a95806977
code-made-a-line <109688894+code-made-a-line@users.noreply.github.com>
2024-12-02 10:47:13 -0600

    Update student added msg box to show student name Form1.cs

--- ExternalData/Form1.cs
+++ ExternalData/Form1.cs
@@ -59,7 +59,7 @@ namespace ExternalData
                     cmd.ExecuteNonQuery();
                 }
             }
-            MessageBox.Show("Added student successfully!");
+            MessageBox.Show("Added student " + student.FirstName + " successfully!");
         }
     }
 }
